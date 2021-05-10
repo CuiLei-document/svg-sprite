c9cf593a97b4a99cc809284db4251bb3cd5e7fe5
磊哥 <493114008@qq.com>
2021-05-10 13:08:23 +0800

    vue.config.js 配置 svg

---
+++ src/views/Money.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script lang='ts'>
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+    import x from '@/assets/icons/money.svg'
+
+    console.log(x);
+    @Component
+    export default class Money extends Vue {
+
+    }
+</script>
+
+<style scoped lang=scss>
+
+</style>
